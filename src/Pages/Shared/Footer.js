548c60bb38c9483c28257b39de3309335b6264c4
mdsohag65 <sohag92ahmed@gmail.com>
2022-07-21 17:06:31 +0600

    footer done

---
+++ src/Pages/Shared/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import footer from '../../assets/images/footer.png'
+
+const Footer = () => {
+    return (
+        <footer style={{
+            background: `url(${footer})`,
+            backgroundSize: 'contain'
+        }} className="p-10 px-12 my-5">
+            <div className='footer'>
+                <div>
+                    <span className="footer-title">SERVICES</span>
+                    <a className="link link-hover">Emergency Checkup</a>
+                    <a className="link link-hover">Monthly Checkup</a>
+                    <a className="link link-hover">Weekly Checkup</a>
+                    <a className="link link-hover">Deep Checkup</a>
+                </div>
+                <div>
+                    <span className="footer-title">ORAL HEALTH</span>
+                    <a className="link link-hover">Fluoride Treatment</a>
+                    <a className="link link-hover">Cavity Filling</a>
+                    <a className="link link-hover">Teath Whitening</a>
+                </div>
+                <div>
+                    <span className="footer-title">OUR ADDRESS</span>
+                    <a className="link link-hover">New York - 101010 Hudson</a>
+                </div>
+            </div>
+            <footer className="footer footer-center p-4">
+                <div>
+                    <p>Copyright © 2022 - All rights reserved</p>
+                </div>
+            </footer>
+        </footer>
+    );
+};
+
+export default Footer;
