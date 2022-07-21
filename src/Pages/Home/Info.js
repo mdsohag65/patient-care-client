5d615c300c66b6e0f435710635a1a1648f516c90
mdsohag65 <sohag92ahmed@gmail.com>
2022-07-21 17:25:57 +0600

    reset the card color

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -8,7 +8,7 @@ const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-6'>
             <InfoCard cardTitle="Opening Hours" cardText="Lorem Ipsum is simply dummy text of the pri" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>
-            <InfoCard cardTitle="Visit our location" cardText="Brooklyn, NY 10036, United States" bgClass="bg-accent" img={marker}></InfoCard>
+            <InfoCard cardTitle="Visit our location" cardText="Brooklyn, NY 10036, United States" bgClass="bg-black" img={marker}></InfoCard>
             <InfoCard cardTitle="Contact us now" cardText="+000 123 456789" bgClass="bg-gradient-to-r from-primary to-secondary" img={phone}></InfoCard>
         </div>
     );
