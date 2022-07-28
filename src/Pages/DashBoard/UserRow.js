11a792d093b3c506ed2d1a39f99fb1b328029a19
mdsohag65 <sohag92ahmed@gmail.com>
2022-07-28 13:30:44 +0600

    connect with heroku

--- src/Pages/DashBoard/UserRow.js
+++ src/Pages/DashBoard/UserRow.js
@@ -5,7 +5,7 @@ const UserRow = ({ user }) => {
     const { email, role } = user;
 
     const makeAdmin = () => {
-        fetch(`http://localhost:5000/user/admin/${email}`, {
+        fetch(`https://limitless-mountain-82756.herokuapp.com/user/admin/${email}`, {
             method: 'PUT',
             headers: {
                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
--- src/Pages/DashBoard/Users.js
+++ src/Pages/DashBoard/Users.js
@@ -4,7 +4,7 @@ import UserRow from './UserRow';
 const Users = () => {
     const [users, setUsers] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:5000/user', {
+        fetch('https://limitless-mountain-82756.herokuapp.com/user', {
             method: 'GET',
             headers: {
                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
